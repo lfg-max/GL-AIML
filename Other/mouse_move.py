f4dd9a008de226f4074bd180dae106c7d20aac63
lfg-max <nra.deeplearning02@gmail.com>
2025-01-03 00:52:46 +0530

    sync changes in main code version

--- Other/mouse_move.py
+++ Other/mouse_move.py
@@ -1,12 +1,16 @@
 import pyautogui
 import random
 import time
+import threading
+import os
 
+stop_flag = threading.Event()
 
 def read_config():
     config = {}
+    config_path = os.path.join(os.path.dirname(__file__), 'config.txt')
     try:
-        with open('config.txt', 'r') as file:
+        with open(config_path, 'r') as file:
             for line in file:
                 name, value = line.strip().split('=')
                 config[name] = int(value)
@@ -14,14 +18,19 @@ def read_config():
         print("Config file not found. Using default values.")
     except ValueError:
         print("Error parsing config file. Using default values.")
+    print(f"Config: {config}")
     return config
 
 def keep_mouse_moving():
     """
     Keeps the mouse cursor moving randomly on the screen to prevent screen savers.
     """
-    while True:
+    
+    while not stop_flag.is_set():
         try:
+            
+            os.system('cls' if os.name == 'nt' else 'clear')
+            
             # Get screen dimensions
             screen_width, screen_height = pyautogui.size()
 
@@ -32,22 +41,25 @@ def keep_mouse_moving():
             # Move the mouse cursor to the random coordinates
             pyautogui.moveTo(x, y, duration=random.uniform(1, 1)) 
 
-            # Read the configuration values again
             config = read_config()
-            wait_time = config.get('wait_time', 10)  # Default to 10 if not found
-
-            # Pause for a random interval
-            time.sleep(random.uniform(1, wait_time)) 
-
-        except OSError as e:
-            if e.errno == 13:
-                print("Error 13 - Must be run as administrator")
-            else:
-                print(f"An OS error occurred: {e}")
-            break
+            wait_time = config.get('wait_time', 10)  # Default wait time is 10 seconds
+            print("\nPress Enter to stop the script...\n")
+            # Print the wait time remaining
+            for remaining in range(wait_time, 0, -1):
+                if stop_flag.is_set():
+                    break
+                print(f"Waiting for {remaining} seconds...", end='\r')
+                time.sleep(1)
         except Exception as e:
             print(f"An error occurred: {e}")
-            break
+
+def listen_for_enter():
+    input("Press Enter to stop the script...\n")
+    stop_flag.set()
 
 if __name__ == "__main__":
+    listener_thread = threading.Thread(target=listen_for_enter)
+    listener_thread.start()
     keep_mouse_moving()
+    listener_thread.join()
+    print("Script stopped.")

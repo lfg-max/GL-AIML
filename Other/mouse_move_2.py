dc02bc6847866fc0f3bf37290e21c196d190cfa4
lfg-max <nra.deeplearning02@gmail.com>
2025-01-03 00:49:25 +0530

    better log and relative file path for config

--- Other/mouse_move_2.py
+++ Other/mouse_move_2.py
@@ -25,8 +25,12 @@ def keep_mouse_moving():
     """
     Keeps the mouse cursor moving randomly on the screen to prevent screen savers.
     """
+    
     while not stop_flag.is_set():
         try:
+            
+            os.system('cls' if os.name == 'nt' else 'clear')
+            
             # Get screen dimensions
             screen_width, screen_height = pyautogui.size()
 
@@ -35,24 +39,19 @@ def keep_mouse_moving():
             y = random.randint(0, screen_height)
 
             # Move the mouse cursor to the random coordinates
-            pyautogui.moveTo(x, y, duration=random.uniform(1, 2)) 
+            pyautogui.moveTo(x, y, duration=random.uniform(1, 1)) 
 
-            # Read the configuration values again
             config = read_config()
-            wait_time = config.get('wait_time', 10)  # Default to 10 if not found
-
-            # Pause for a random interval
-            time.sleep(random.uniform(1, wait_time)) 
-
-        except OSError as e:
-            if e.errno == 13:
-                print("Error 13 - Must be run as administrator")
-            else:
-                print(f"An OS error occurred: {e}")
-            break
+            wait_time = config.get('wait_time', 10)  # Default wait time is 10 seconds
+            print("\nPress Enter to stop the script...\n")
+            # Print the wait time remaining
+            for remaining in range(wait_time, 0, -1):
+                if stop_flag.is_set():
+                    break
+                print(f"Waiting for {remaining} seconds...", end='\r')
+                time.sleep(1)
         except Exception as e:
             print(f"An error occurred: {e}")
-            break
 
 def listen_for_enter():
     input("Press Enter to stop the script...\n")
